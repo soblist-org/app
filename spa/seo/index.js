7685daf86e03c5204b4be3f1eda62a61a77afa80
soblist <soblist@outlook.com>
2019-05-23 16:37:06 +0000

    update seo scripts

--- spa/seo/index.js
+++ spa/seo/index.js
@@ -1,28 +1,47 @@
+'use strict'
+const axios = require('axios');
 const hb = require('handlebars');
 const fs = require('fs');
 
 // require relative
-const sobs = require('./soblist.json');
-// read project root
-const tpl = hb.compile(fs.readFileSync('./seo/tpl/index.tpl').toString());
-const sitemapTpl = hb.compile(
-  fs.readFileSync('./seo/tpl/sitemap.tpl').toString()
-);
+axios
+    .get('https://raw.githubusercontent.com/soblist-org/list/master/soblist.json')
+    .then((res) => {
+        const sobs = res.data;
+        // read project root
+        const tpl = hb.compile(fs.readFileSync('./seo/tpl/index.tpl').toString());
+        const sitemapTpl = hb.compile(
+            fs.readFileSync('./seo/tpl/sitemap.tpl').toString()
+        );
 
-const prefix = 'https://www.soblist.org/raw/';
-const urls = [];
+        const prefix = 'https://www.soblist.org/raw/';
+        const urls = [];
 
-fs.mkdirSync('./dist/raw');
+        fs.mkdirSync('./dist/raw');
 
-for (let i = 0; i < sobs.length; i++) {
-  const fileName = 'sob_' + i + '.html';
-  const lastmod = sobs[i].addedDate;
+        for (let i = 0; i < sobs.length; i++) {
+            const fileName = 'sob_' + i + '.html';
+            const lastmod = sobs[i].addedDate;
 
-  fs.writeFileSync('./dist/raw/' + fileName, tpl(sobs[i]));
-  urls.push({
-    url: prefix + fileName,
-    lastmod,
-  });
-}
+            fs.writeFileSync('./dist/raw/' + fileName, tpl(sobs[i]));
+            urls.push({
+                url: prefix + fileName,
+                lastmod,
+            });
+        }
 
-fs.writeFileSync('./dist/' + 'sitemap.xml', sitemapTpl({ urls, now: new Date().toISOString() }));
+        fs.writeFileSync(
+            './dist/' + 'sitemap.xml',
+            sitemapTpl({ urls, now: new Date().toISOString() })
+        );
+    })
+    .catch((e) => {
+        console.error(
+            'Exception raised when trying to fetch soblist from github raw content.',
+            e
+        );
+    })
+    .finally(() => {
+        console.info('request soblist finished.');
+    });
+
