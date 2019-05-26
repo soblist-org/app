49077a73e6482ac6ba2ac4fb2ba0d33942a57896
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-26 12:21:39 +0000

    Deploy soblist-org/app to github.com/soblist-org/app.git:gh-pages

---
+++ precache-manifest.949899b4fcddd92e60335b79972abaee.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a3c1a922a30fc2c4dad",
+    "url": "/js/about.f851369a.js"
+  },
+  {
+    "revision": "e51a8fae1dff703013c1",
+    "url": "/css/app.38e41eda.css"
+  },
+  {
+    "revision": "e51a8fae1dff703013c1",
+    "url": "/js/app.2f1e3b0e.js"
+  },
+  {
+    "revision": "32f5f219fa73bf72a3cf",
+    "url": "/css/chunk-vendors.12b8badc.css"
+  },
+  {
+    "revision": "32f5f219fa73bf72a3cf",
+    "url": "/js/chunk-vendors.86d2106f.js"
+  },
+  {
+    "revision": "b0b8902b4bc235a1e0aaa874ba66531e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85a0fc3af791c91959e351cc9d236040",
+    "url": "/BingSiteAuth.xml"
+  },
+  {
+    "revision": "9f3ebf0d3fabf8acee4749b07f09311c",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "71ce18f1a23958639f912dbc5ea3833a",
+    "url": "/google9f140c651b7c56ae.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d18c724eff782a1faab9158b9a390d83",
+    "url": "/yandex_39885693835721eb.html"
+  }
+];
